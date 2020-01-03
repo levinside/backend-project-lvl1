756a2f820997791f6a01359bce5f228696628269
Lev Levin <levinside@gmail.com>
2020-01-04 01:51:33 +0300

    Step3

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 
-console.log("Welcome to the Brain Games!");
+import askName from '..';
 
+console.log("Welcome to the Brain Games!\n");
+
+askName();
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// ``
+
+import readlineSync from 'readline-sync';
+
+const askName = () => {
+  const actual = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${actual}!`);
+}
+
+export default askName;
